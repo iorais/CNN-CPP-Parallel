405f4a76e7d5df1c6998eea8dbbcec72c88b5495
Wesley Durbano <wdurbano@scu.edu>
2024-12-05 00:03:51 -0800

    24 cpus, log parse

--- Parallel-OMP/job.sh
+++ Parallel-OMP/job.sh
@@ -5,7 +5,7 @@
 #SBATCH --partition=cmp
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=32
 #SBATCH --mem=32G
 #SBATCH --export=ALL
 #SBATCH -t 08:00:00
@@ -19,7 +19,7 @@
 
 module load GCC
 make
-for threads in {1,2,4,8,12,14,16,18,20,22,24}; do
+for threads in {1,2,3,4,5,6,7,8,12,14,16,18,20,22,24}; do
   export OMP_NUM_THREADS=$threads
   bin/CNN --num_epochs 1 --preview_period 100000
 done
